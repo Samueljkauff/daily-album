d3f10f00b6bcf63f3860b50013827bdfd3192b9a
Sam Kauffman <samueljkauff@gmail.com>
2025-12-22 18:16:47 -0500

    Got rid of unused code

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,14 +1,4 @@
 import { User } from "@/interfaces/user_interface";
-import type { Token } from "../interfaces/token_interface";
-
-export const createToken = async (data: Token) => {
-  const res = await fetch("http://localhost:2121/api/token/create", {
-    method: "POST",
-    headers: { "Content-Type": "appliction/json" },
-    body: JSON.stringify({ data }),
-  });
-  return res.json();
-};
 
 export const getUser = async (jwt: string): Promise<User> => {
   const res = await fetch("http://localhost:2121/api/user/profile", {
