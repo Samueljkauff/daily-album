c62414226611a42a39fcfd38f32539de97005e4d
Sam Kauffman <samueljkauff@gmail.com>
2025-12-30 15:42:21 -0500

    minor changes

--- api/src/routes/auth_routes.ts
+++ api/src/routes/auth_routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import * as SpotifyController from "../controllers/auth_controller.js";
+import * as authMiddleware from "../middleware/auth.middleware.js";
 
 const router = Router();
 
