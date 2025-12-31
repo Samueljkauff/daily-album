7c9666e3aa567f6ac7ef59fc692e78c58a8bc20a
Sam Kauffman <samueljkauff@gmail.com>
2025-12-31 04:10:53 -0500

    started middleware

---
+++ api/src/auth/auth_middleware.ts
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken"
+import type { Response, Request, NextFunction } from "express"
+
+export interface AuthenticatedRequest extends Request {
+  auth?: {
+    userId: string;
+    deviceId: string;
+  };
+}
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+if(!JWT_SECRET) {
+    throw new Error("No JWT Secret found.");
+}
+
+export function requireAuth(req: Request, res: Response, next: NextFunction) {
+
+}
